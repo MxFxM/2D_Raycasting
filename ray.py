d9d2de4143af969f2fd90a787000d5950c4830c9
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-24 17:07:40 +0200

    ray

--- ray.py
+++ ray.py
@@ -1,3 +1,16 @@
+import math
+
+WHITE = (255, 255, 255)
+
+
 class Ray:
-    def __init__(self):
-        pass
+
+    def __init__(self, x1, y1, a):
+        self.position = (x1, y1)
+        x_part = math.cos(2 * pi * a / 360)
+        y_part = math.sin(2 * pi * a / 360)
+        self.direction = (x_part, y_part)
+
+    def show(self, surface):
+        pygame.draw.line(surface, WHITE, self.position, (round((p+d)*10)
+                                                         for p, d in zip(self.position, self.direction)))
