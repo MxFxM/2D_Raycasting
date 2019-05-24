7cf2fe958307c2e94741dee59e103e963846a7b4
MxFxM <32713448+MxFxM@users.noreply.github.com>
2019-05-24 16:54:40 +0200

    boundary

--- boundary.py
+++ boundary.py
@@ -1,3 +1,18 @@
+import pygame
+
+
+WHITE = (255, 2555, 255)
+
+
 class Boundary:
-    def __init__(self):
-        pass
+
+    def __init__(self, x1, y1, x2, y2):
+        self.a_pos = (x1, y1)
+        self.b_pos = (x2, y2)
+
+    def show(self, surface):
+        pygame.draw.line(surface, BLACK, a_pos, b_pos)
+
+
+if __name__ == '__main__':
+    print("Please run raycasting.py as main.")
